ab7180f6b3983bc8802db6b66726b85134952aa1
caiordm <caiordm08@gmail.com>
2024-04-04 22:12:00 -0300

    resolving postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_04_03_000219) do
-  create_table "urls", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "urls", force: :cascade do |t|
     t.string "url_original"
     t.string "url_hash"
     t.datetime "created_at", null: false
